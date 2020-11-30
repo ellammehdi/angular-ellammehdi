a6e2ef06eb74a41104d17d8a4de744650824a28d
ellammehdi <ellamraniimehdi@gmail.com>
2020-11-30 01:50:10 +0100

    2 files modified

--- src/app/features/my-first-component/my-first-component.component.ts
+++ src/app/features/my-first-component/my-first-component.component.ts
@@ -1,27 +1,38 @@
 import { Component, OnInit, ViewChild, ElementRef } from "@angular/core";
 import { Informations, PonyModel } from "./informations";
-import { FormGroup, FormControl, FormBuilder } from "@angular/forms";
+import {
+  FormGroup,
+  FormControl,
+  FormBuilder,
+  Validators
+} from "@angular/forms";
 @Component({
   selector: "app-my-first-component",
   templateUrl: "./my-first-component.component.html",
   styleUrls: ["./my-first-component.component.scss"]
 })
 export class MyFirstComponentComponent implements OnInit {
-  LogForm: FormGroup;
+  identityFormGroup: FormGroup;
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit() {
+    this.identityFormGroup = this.fb.group({
+      firstName: ["", Validators.required],
+      lastName: "",
+      email: "",
+      password: ""
+    });
+  }
 
   onSubmit() {
-    console.log(this.ProfileControl.value);
+    console.log("lkjlkj");
   }
   UpdateProfile() {
     this.ProfileControl.setValue({
       firstName: "Your FirstName",
       lastName: "Your LastName",
       email: "Your Email",
-      password: "Your Password",
-      Address: {
-        city: "Your City",
-        CodePostal: "Code Postal"
-      }
+      password: "Your Password"
     });
   }
   UpdateLogPass() {}
@@ -30,25 +41,6 @@ export class MyFirstComponentComponent implements OnInit {
     firstName: new FormControl(),
     lastName: new FormControl(),
     email: new FormControl(),
-    password: new FormControl(),
-    Address: new FormGroup({
-      city: new FormControl(),
-      CodePostal: new FormControl()
-    })
+    password: new FormControl()
   });
-  poniesDict = new Map<number, PonyModel>();
-
-  ponies: Array<any> = [
-    { name: "Rainbow Dash" },
-    { name: "Pinkie Pie" },
-    { name: "Fluttershy" },
-    { name: "Rarity" }
-  ];
-
-  birthDate = Date.now();
-
-  stringInfo = "";
-  newInfo: Informations;
-  newInfo2: Informations;
-  ngOnInit() {}
 }
